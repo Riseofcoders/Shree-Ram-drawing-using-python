2c3452603c1316cc1eb94b1bc8a8f91541e7ab4a
Riseofcoders <103364011+Riseofcoders@users.noreply.github.com>
2023-03-29 18:52:29 +0530

    Update ShreeRam.py

--- ShreeRam.py
+++ ShreeRam.py
@@ -21,7 +21,7 @@ def outline():
                                 thresholdType=cv2.THRESH_BINARY, blockSize=9, C=2)
     return th3
 
-image = 'ramsita.jpg'
+image = 'Jai-Shree-Ram-thumb.png'
 im = cv2.imread(image, 0)
 th3 = outline()
 
